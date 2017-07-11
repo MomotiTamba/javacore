789997f7592d2f7308f0007da9a9c757aeae7462
lesson2 <achilles910@gmail.com>
2017-07-11 12:28:57 +0300

    Amend homework16:
    -fix call objects
    
    Now the program work correct

--- src/main/java/com/kitcenter/runners/homework/lesson16/HomeTask16Runner.java
+++ src/main/java/com/kitcenter/runners/homework/lesson16/HomeTask16Runner.java
@@ -13,6 +13,7 @@ public class HomeTask16Runner {
         crawlerTractor.setPassenger(4);
         crawlerTractor.setWeight(2000);
         crawlerTractor.setWheel(0);
+
         System.out.println(wheeledTractor);
         System.out.println(crawlerTractor);
     }
