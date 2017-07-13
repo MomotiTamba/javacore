3a377b262110bb2bea576a6414a349a5072b786f
lesson2 <achilles910@gmail.com>
2017-07-13 13:38:57 +0300

    Amend homework21: - add class WebDriverTetBase - improve class StylusSeleniumTest

--- src/test/java/com/kitcenter/app/homework/lesson21/StylusSeleniumTest.java
+++ src/test/java/com/kitcenter/app/homework/lesson21/StylusSeleniumTest.java
@@ -1,30 +1,28 @@
 package com.kitcenter.app.homework.lesson21;
 
-
-import org.junit.Assert;
-import org.junit.Test;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.WebDriverRunner;
+import com.kitcenter.app.core.selenium.WebDriverTestBase;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Point;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeDriver;
-
+import org.testng.annotations.Test;
 import java.util.List;
-import java.util.concurrent.TimeUnit;
 
-public class StylusSeleniumTest {
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+import static org.testng.Assert.assertTrue;
+
+public class StylusSeleniumTest extends WebDriverTestBase {
 
     private String stylusSearch = "https://stylus.ua";
     private String searchText = "Sony Z2";
-    private WebDriver webDriver;
     List<WebElement> elements;
 
     @Test
     public void searchPhoneTest(){
-        System.setProperty("webdriver.chrome.driver", "C:\\Users\\muon\\KIT\\chromedriver_win32\\chromedriver.exe");
-        webDriver = new ChromeDriver();
-        webDriver.manage().window().maximize();
-        webDriver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         webDriver.get(stylusSearch);
         By searchLocator = By.cssSelector("div[id='head-search'] input[type='text']");
         WebElement searchField = webDriver.findElement(searchLocator);
@@ -36,13 +34,22 @@ public class StylusSeleniumTest {
         By openedLinkLocator = By.xpath(".//*[@id='product-block']/header/div[1]/h1");
         elements = webDriver.findElements(searchLinkLocator);
         for (WebElement webElement : elements){
-            Assert.assertTrue(webElement.isDisplayed());
+            assertTrue(webElement.isDisplayed());
             webElement.click();
         }
         elements = webDriver.findElements(openedLinkLocator);
         for (WebElement webElement : elements){
-            Assert.assertTrue(webElement.isDisplayed());
+            assertTrue(webElement.isDisplayed());
         }
-        webDriver.quit();
+    }
+
+    @Test
+    public void searchPhoneTest1(){
+        Configuration.browser = WebDriverRunner.CHROME;
+        open(stylusSearch);
+        $("div[id='head-search'] input[type='text']").val(searchText);
+        $("div[id='head-search'] input[type='submit']").click();
+        $(By.xpath(".//*[@id='search-list']/ul/li[5]/a/span")).shouldBe(visible).click();
+        $(By.xpath(".//*[@id='product-block']/header/div[1]/h1")).shouldBe(visible).shouldHave(text("Смартфон Sony Xperia Z2 Black"));
     }
 }
